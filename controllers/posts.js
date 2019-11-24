d305927dc3e207024e359e4776784b4f2ea31cb0
OrigamiOracle <oo@origamioracle.com>
2019-11-24 11:00:06 +1100

    Added Destroy route

--- controllers/posts.js
+++ controllers/posts.js
@@ -29,5 +29,10 @@ module.exports = {
 	async postUpdate(req, res, next) {
 		let post = await Post.findByIdAndUpdate(req.params.id, req.body.post, { new: true });
 		res.redirect(`/posts/${post.id}`);
+	},
+	// Posts destroy
+	async postDestroy(req, res, next) {
+		await Post.findByIdAndRemove(req.params.id, req.body.post);
+		res.redirect('/posts/');
 	}
 }
